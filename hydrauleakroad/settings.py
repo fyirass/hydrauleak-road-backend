40e0f8f91d5eea5a68e91ee9dd506bdfaacca655
fyirass <firas.mansour@esprit.tn>
2023-01-19 02:47:55 +0100

    add useraccounts

--- hydrauleakroad/settings.py
+++ hydrauleakroad/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cd$o(krth9w9*weuc_(h4ptjorw_*aq1jeikbiy+e+z!8dgb7#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
