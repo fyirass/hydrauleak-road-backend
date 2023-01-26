9fd47ee1d68d7d3be80cd6f6d5591d3f6ffa352e
fyirass <firas.mansour@esprit.tn>
2023-01-26 16:40:34 +0100

    add id

--- contracts/serializers.py
+++ contracts/serializers.py
@@ -4,8 +4,8 @@ from user.models import User
 from clients.serializers import ClientSerializer
 
 class ContractSerializer(serializers.ModelSerializer):
-    client = ClientSerializer(read_only=True)
+    client_detail = ClientSerializer(read_only=True)
     class Meta:
         model = Contract
-        fields = ('client','id', 'zone', 'contract_title', 'contract_description', 'contract_type',
+        fields = ('client_detail','client','id', 'zone', 'contract_title', 'contract_description', 'contract_type',
                     'contract_status', 'contract_work_type', 'contract_date', 'address', 'city', 'state', 'zipcode', 'is_published')
