3668b4a59316af1544cf460ac0f20fa740bdb57a
fyirass <firas.mansour@esprit.tn>
2023-01-26 14:01:17 +0100

    add user name with contracts

--- clients/serializers.py
+++ clients/serializers.py
@@ -5,7 +5,7 @@ from user.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'name', 'email', 'phone')
+        fields = ('id','roles', 'name', 'email', 'phone')
 
 class ClientSerializer(serializers.ModelSerializer):
     user = UserSerializer()
