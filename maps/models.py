f6e2f036ac56849ce03160003294b9c264711fe0
fyirass <firas.mansour@esprit.tn>
2023-02-01 19:06:41 +0100

    add zone area

--- maps/models.py
+++ maps/models.py
@@ -17,6 +17,8 @@ class Zone(models.Model):
     
     id = models.AutoField(primary_key=True)
     map = models.ForeignKey(Map, on_delete=models.SET_NULL, related_name="maps", null=True, blank=True)
+    zone_title = models.CharField(max_length=100, blank=True)
+    zone_description = models.TextField(blank=True)
     zone_num = models.IntegerField(default=0)
     zone_date = models.DateTimeField(default=datetime.now)
     zone_status = models.CharField(max_length=20,choices=[('notStart', 'Not Started'), ('Pending', 'Pending'), ('Completed', 'Completed')], default= "notStart")
