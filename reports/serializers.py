f52e47a9b30ca3fe2089c9a02eed3298c300ab77
fyirass <firas.mansour@esprit.tn>
2023-02-16 17:12:39 +0100

    fix requres reports

--- reports/serializers.py
+++ reports/serializers.py
@@ -16,8 +16,8 @@ class ReportSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Report
-        fields = ('user_name', 'user_role','id', 'subject', 'message', 'add_sensor_coordinates', 'add_mark_coordinates', 'add_pipe_coordinates', 'add_pipe_access_coordinates'
-                  'image', 'report_date', 'leaker',                 
+        fields = ('user','user_name', 'user_role','id', 'subject', 'message', 'add_sensor_coordinates', 'add_mark_coordinates', 'add_pipe_coordinates', 'add_pipe_access_coordinates',
+                  'image', 'report_date'                 
                   )
         
         
