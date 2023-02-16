d01e3fd971253aae10f7ba81594ee7dd1258d0e2
fyirass <firas.mansour@esprit.tn>
2023-02-17 00:13:23 +0100

    fix report send mail

--- reports/serializers.py
+++ reports/serializers.py
@@ -4,21 +4,14 @@ from user.serializers import UserSerializer
 
 class ReportSerializer(serializers.ModelSerializer):
     
-    user = UserSerializer(read_only=True)
-    user_name = serializers.SerializerMethodField()
-    user_role = serializers.SerializerMethodField()
-
-    def get_user_name(self, obj):
-        return obj.user.name
-
-    def get_user_role(self, obj):
-        return obj.user.roles
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     
     class Meta:
         model = Report
-        fields = ('user','user_name', 'user_role','id', 'subject', 'message', 'add_sensor_coordinates', 'add_mark_coordinates', 'add_pipe_coordinates', 'add_pipe_access_coordinates',
+        fields = ('user','id', 'subject', 'message', 'add_sensor_coordinates', 'add_mark_coordinates', 'add_pipe_coordinates', 'add_pipe_access_coordinates',
                   'image', 'report_date'                 
                   )
-        
-        
-        
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
--- reports/views.py
+++ reports/views.py
@@ -26,28 +26,33 @@ class ReportViewSet(viewsets.ModelViewSet):
         add_pipe_coordinates = self.request.data.get('add_pipe_coordinates')
         add_pipe_access_coordinates = self.request.data.get('add_pipe_access_coordinates')
         report_date = self.request.data.get('report_date')
+        user = self.request.user
         image = self.request.data.get('image')
-        
-        
+
         try:
-            admin = User.objects.filter(roles="is_admin").first()
+            admin = User.objects.filter(roles="is_admin")[1]
             admin_email = admin.email
+            print ('admin email: ' + admin_email)
         except User.DoesNotExist:
             admin_email = None
-        
-        
+
         message += '\n\nAdditional sensor coordinates: ' + str(add_sensor_coordinates)
         message += '\n\nAdditional mark coordinates: ' + str(add_mark_coordinates)
         message += '\n\nAdditional pipe coordinates: ' + str(add_pipe_coordinates)
         message += '\n\nAdditional pipe access coordinates: ' + str(add_pipe_access_coordinates)
         message += '\n\nReport date: ' + str(report_date)
-        
-        
-        # with open(image.path, 'rb') as f:
-        #     img_data = f.read()
-        #     image = MIMEImage(img_data, name=image.name)
-                    # .attach(image)
-            
+
+        report = Report.objects.create(
+            user=user,
+            subject=subject,
+            message=message,
+            add_sensor_coordinates=add_sensor_coordinates,
+            add_mark_coordinates=add_mark_coordinates,
+            add_pipe_coordinates=add_pipe_coordinates,
+            add_pipe_access_coordinates=add_pipe_access_coordinates,
+            report_date=report_date
+        )
+
         if self.request.user.roles =="is_client":
             message ='Message from client: '+ message
             send_mail(subject, message, self.request.user.client.user.email, [admin_email])
