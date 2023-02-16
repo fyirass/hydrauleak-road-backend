2bc4c2a162b17c48546ebe177f6b42ffd8068043
fyirass <firas.mansour@esprit.tn>
2023-02-17 00:21:11 +0100

    fix report send mail

--- reports/serializers.py
+++ reports/serializers.py
@@ -5,10 +5,18 @@ from user.serializers import UserSerializer
 class ReportSerializer(serializers.ModelSerializer):
     
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    user_name = serializers.SerializerMethodField()
+    user_role = serializers.SerializerMethodField()
+
+    def get_user_name(self, obj):
+        return obj.user.name
+
+    def get_user_role(self, obj):
+        return obj.user.roles
     
     class Meta:
         model = Report
-        fields = ('user','id', 'subject', 'message', 'add_sensor_coordinates', 'add_mark_coordinates', 'add_pipe_coordinates', 'add_pipe_access_coordinates',
+        fields = ('user', 'user_name', 'user_role','id', 'subject', 'message', 'add_sensor_coordinates', 'add_mark_coordinates', 'add_pipe_coordinates', 'add_pipe_access_coordinates',
                   'image', 'report_date'                 
                   )
 
