fea4e565988e99151ecc36f40eef680fd66c672a
fyirass <firas.mansour@esprit.tn>
2023-02-25 23:05:12 +0100

    fix clients

--- clients/serializers.py
+++ clients/serializers.py
@@ -1,17 +1,19 @@
+# serializers.py
+
 from rest_framework import serializers
 from .models import Client
-from user.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id','roles', 'name', 'phone')
 
 class ClientSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Client
-        fields = ('user','id', 'photo', 'description',  'address',  'inscription_date', 'client_files')
-        
+        fields = ('id', 'user', 'photo', 'description', 'address', 'inscription_date', 'client_files')
+        read_only_fields = ('id',)
 
-        
+
+class ClientWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ('user', 'photo', 'description', 'address', 'inscription_date', 'client_files')
--- clients/views.py
+++ clients/views.py
@@ -1,13 +1,55 @@
+
 from rest_framework import viewsets
-from .models import Client
-from .serializers import ClientSerializer
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status, permissions
+from .serializers import ClientSerializer, ClientWriteSerializer
+from .models import Client, ClienTFile, User
+
 
 class ClientViewSet(viewsets.ModelViewSet):
-    
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = None
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'update', 'partial_update'):
+            return ClientWriteSerializer
+        return ClientSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.select_related('user')
+
+    def create(self, request, *args, **kwargs):
+        user_id = request.data.get('user')
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response(f"User with id {user_id} does not exist", status=400)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    @action(methods=['get'], detail=True)
+    def full(self, request, pk=None):
+        client = self.get_object()
+        serializer = ClientSerializer(client, context={'request': request})
+        return Response(serializer.data, status=200)
+
+    @action(methods=['post'], detail=True)
+    def add_file(self, request, pk=None):
+        client = self.get_object()
+        file_obj = request.FILES.get('file')
+        client_file = ClienTFile.objects.create(Client_file=file_obj)
+        client.client_files.add(client_file)
+        serializer = ClientSerializer(client, context={'request': request})
+        return Response(serializer.data, status=200)
+
+    @action(methods=['delete'], detail=True)
+    def remove_file(self, request, pk=None):
+        client = self.get_object()
+        file_id = request.query_params.get('file_id')
+        client.client_files.filter(id=file_id).delete()
+        serializer = ClientSerializer(client, context={'request': request})
+        return Response(serializer.data, status=200)
