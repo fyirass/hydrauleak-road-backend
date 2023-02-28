4f8096ba4c29fb04e917b186c491ff3ded800da0
fyirass <firas.mansour@esprit.tn>
2023-02-28 13:04:25 +0100

    fiz contract zone

--- contracts/serializers.py
+++ contracts/serializers.py
@@ -7,7 +7,7 @@ class ContractSerializer(serializers.ModelSerializer):
     client = ClientSerializer(read_only=True)
     class Meta:
         model = Contract
-        fields = ('client','id', 'zone', 'contract_title', 'contract_description', 'contract_type',
+        fields = ('client','id', 'contract_title', 'contract_description', 'contract_type',
                     'contract_status', 'contract_work_type', 'contract_date', 'contract_estimate_end_date', 'address', 'city', 'state', 'zipcode', 'is_published')
 
 class ClientContractSerializer(serializers.ModelSerializer):
